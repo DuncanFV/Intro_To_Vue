1d37f3353045b2b8baea7661ff9d0e011f31c893
Duncan Ferency-Viars <77799251+DuncanFV@users.noreply.github.com>
2021-02-19 20:49:28 -0500

    finished

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,7 +1,14 @@
 app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+
     template: 
    /*html*/
-    '<div class="product-display">
+    `<div class="product-display">
         <div class="product-container">
           <div class="product-image">
             <img :src="image">
@@ -10,12 +17,14 @@ app.component('product-display', {
             <h1>{{ title }}</h1>
             <p>{{ description }}</p>
             <p v-if="inStock">In Stock</p>
-            <!-- <p v-else-if="inventory <=10 && inventory>0">Almost Sold Out</p> -->
             <p v-else>Out Of Stock</p>
+
+            <p>Shipping: {{ shipping }}</p>
             <p>{{ onSaleDisplay }}</p>
             <ul>
               <li v-for="detail in details">{{ detail }}</li>
             </ul>
+            
             <div 
               v-for="(variant, index) in variants"
               :key="variant.id"
@@ -23,7 +32,7 @@ app.component('product-display', {
               class="color-circle"
               :style="{ backgroundColor: variant.color }">
             </div>
-            <!-- <ul> <li v-for="size in sizes">{{ size }}</li> </ul> -->
+
             <button
               class="button"
               :class="{ disabledButton: !inStock }"
@@ -31,11 +40,81 @@ app.component('product-display', {
               @click="addToCart">
               Add to Cart
             </button>
-            <!-- <button class="button" @click="removeFromCart">Remove from Cart</button> -->
+
+            <button 
+              class="button"
+              :class="{ disabledButton: !inStock }"
+              :disabled="!inStock"
+              @click="deleteCart">
+              Remove Item
+            </button>
+
             <p>
               <a :href="url">Mystery Link</a>
             </p>
           </div>
         </div>
-      </div>'
+        <review-list v-if="reviews.length":reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
+      </div>`,
+
+
+      data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            description: 'Wear on feet',
+            selectedVariant: 0,
+            url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+            //inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: false},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false},
+            ],
+            sizes: ['Small', 'Medium', 'Large'],
+            reviews: []
+        }
+    },
+    
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+        },
+
+        deleteCart() {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+        },
+
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
+        }
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSaleDisplay() {   //currently both set to false so it won't show
+            if (this.variants[this.selectedVariant].onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
+        },
+        shipping() {
+            if (this.premium) {
+                return 'Free'
+            }
+            return '$2.99'
+        }
+    }
 })
--- main.js
+++ main.js
@@ -1,52 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            description: 'Wear on feet',
-            selectedVariant: 0,
-            url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-            //inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: false},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false},
-            ],
-            sizes: ['Small', 'Medium', 'Large']
+            cart: [],
+            premium: true
         }
     },
     
     methods: {
-        addToCart() {
-            this.cart += 1
-            //inventory -= 1 //doesn't work
+        updateCart(id) {
+            this.cart.push(id)
         },
-
-        removeFromCart() {
-            this.cart -= 1
-            //inventory += 1
-        },
-
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        onSaleDisplay() {   //currently both set to false so it won't show
-            if (this.variants[this.selectedVariant].onSale) {
-                return this.brand + ' ' + this.product + ' is on sale.'
+        deleteCart(id) {
+            const length = this.cart.indexOf(id)
+            if (length > -1) {
+                this.cart.splice(length, 1)
             }
-            return ''
         }
     }
 })
